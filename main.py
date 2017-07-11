49314cba18668c27cfb2b113188a11ac602dbeac
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-07-11 10:51:12 +0200

    query

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import data_manager
 
 app = Flask('codecool_series')
@@ -7,6 +7,17 @@ app = Flask('codecool_series')
 @app.route('/')
 def index():
     result = data_manager.execute_select('SELECT id, title FROM shows;')
+    return render_template('index.html', shows=result)
+
+
+
+@app.route('/<show_id>/<season_number>/')
+def episodes(show_id, season_number):
+    query = """SELECT episodes.episode_number, episodes.title 
+             FROM episodes JOIN seasons ON seasons.id = episodes.season_id JOIN shows ON seasons.show_id=shows.id 
+             WHERE shows.id = %s AND seasons.season_number= %s;"""
+    values = (shows_id, season_number, )
+    result = data_manager.execute_select(query, values)
     return render_template('index.html', shows=result)
 
 
